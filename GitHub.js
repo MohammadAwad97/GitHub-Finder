fd5e9e069fd56fdffca72e36a29377d61c31ae21
MohammadAwad97 <mohamad.s.awad97@gmail.com>
2023-05-28 22:36:33 +0300

    Update

--- GitHub.js
+++ GitHub.js
@@ -15,11 +15,24 @@ function fetchData() {
         replaceData(data);
         console.log(data)
         console.log(data1);
+        console.log(`Array of allDivs`);
+        let Divs = document.querySelectorAll(".repos-Card");
+        let arrDivs = Array.from(Divs);
+        console.log(arrDivs);
+
+        console.log(`||||| After Slice |||||`);
+        let slicedArr = arrDivs.slice(parseInt (data["public_repos"]) , 7);
+        console.log(slicedArr);
+        
+        console.log(`======================`)
         let x = document.querySelectorAll(".link");
         console.log(x);
         let y = document.querySelectorAll(`.dot_title`);
         console.log(y);
-        if(parseInt (data["public_repos"]) < 6) { // لو كانت عدد المشاريع اقل من 6 , بس انتبه انو لازم تحولها  لانتجر 
+        if(parseInt(data["public_repos"]) < 6) { // لو كانت عدد المشاريع اقل من 6 , بس انتبه انو لازم تحولها  لانتجر 
+            slicedArr.forEach(element => {
+                element.remove();
+            });
             for(let i = 0; i < parseInt (data["public_repos"]) ; i++) {
                 x[i].innerHTML = data1[i]["name"]
             y[i].innerHTML =  `<span class="dot"></span> ${data1[i]["language"]}`
@@ -64,5 +77,30 @@ userName.addEventListener('keypress' , (e) => {
 
 
 
+console.log(`====================================`)
 
+let x = document.querySelectorAll(".link");
+console.log(x);
+
+x.forEach(element => {
+    // element.remove();
+});
+
+let arr = document.querySelectorAll(".repos-Card");
+console.log(arr);
+
+arr.forEach(element => {
+    // element.remove();
+})
+
+
+// Very Important ::::::: 
+
+// let newArr = Array.from(arr);
+// console.log(newArr);
+
+// let a1 = newArr.slice(1 , 3); 
+// a1.forEach(element => {
+//     element.remove();
+// });
 
